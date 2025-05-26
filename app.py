b64945c4dd7b66541dd44f52e3907e377fe678af
phillycodes69 <phillipbond46@gmail.com>
2025-05-26 23:00:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -199,8 +199,7 @@ if st.button("Predict Tomorrow's Price"):
             filename = f"{coin}_history.csv"
             save_data_to_csv(data, filename)
             predicted_prices = predict_price_from_csv(filename)
-            st.write("DEBUG >>> predicted_prices[0]:", predicted_prices[0])
-
+            days, predicated_price = predicted_prices[0]
             st.success(f"Predicted price for tomorrow: ${predicted_prices[0][1]:,.2f}")
             full_data = load_data_for_graph(filename)
             plot_prediction(full_data, predicted_prices)
