7d729935a4bb952a93df8e04365a598f23c9dce9
phillycodes69 <phillipbond46@gmail.com>
2025-05-27 16:10:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ prediction_days = st.sidebar.slider(
 )
 
 # News API key
-NEWS_API_KEY = "your_newsapi_key_here"
+NEWS_API_KEY = "cd069f7a560a4350b78974c71eedbf53"
 
 @st.cache_data(ttl=600)
 def get_economic_news():
